411dc9d872ca2bce919b0aae6805c8f5b0f7d3dc
Priti Saxena <saxenapriti1008@gmail.com>
2026-02-11 15:43:02 +0100

    added task from 4 to 8
    added all necessary files and task.

--- index.ts
+++ index.ts
@@ -2,6 +2,8 @@
 //const helloFunction = () => console.log("Hej!");
 
 // Task -1
+/*Sample Code (Fix this):
+ let greet = firstName => 'Hello ' + firstName greet('Student') Checklist to complete: Add braces {} and return statement Use const Add semicolons Use template literals Fix spacing*/
 
 const greet = (firstName: string): string => {
   return `Hello ${firstName}`;
@@ -24,5 +26,54 @@ const isEven = (num: number): boolean => {
 };
 
 console.log(`Is even: ${isEven(4)}`);
+
+// Task -4 
+
+const square = (x: number): number => {
+  return x * x;
+};
+
+console.log(`Square: ${square(5)}`);
+
+// Task -5
+
+const getAge = (year: number): number => {
+  return 2026 - year;
+};
+
+console.log(`Calculated age: ${getAge(2000)}`);
+
+// Task -6
+
+const prices = [10, 20, 30];
+let total = 0;
+
+prices.forEach((p) => {
+  total += p;
+});
+
+console.log(`Total price: ${total}`);
+
+
+// Task -7
+
+const user = { name: 'John' };
+
+const sayHi = (): void => {
+  console.log(`Hi ${user.name}`);
+};
+
+sayHi();
+
+// Task -8 
+
+const colors = ['red', 'blue'];
+
+colors.forEach((c) => {
+  console.log(`Color: ${c}`);
+});
+
+
+
 
 
